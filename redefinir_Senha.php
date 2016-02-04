5415ce93edf9d53d46f97e022cf9ba0d8c2716a2
Paulo H <Paulo H@192.168.1.2>
2016-02-04 20:36:42 -0200

    Iniciativa PadBook em versão Alfa

---
+++ redefinir_Senha.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Página Inicial</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="css/estiloLogin.css" type="text/css" rel="stylesheet">
+
+    </head>
+    <body>
+	
+	</body>
+</html>	
