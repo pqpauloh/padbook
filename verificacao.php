2a9b15f3f9519c1e64d5452b5e832b33d05582d8
Paulo H <Paulo H@192.168.1.2>
2016-02-04 20:36:59 -0200

    Iniciativa PadBook em versão Alfa

---
+++ verificacao.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Página Inicial</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="css/estiloLogin.css" type="text/css" rel="stylesheet">
+
+    </head>
+    <body>
+
+        <?php
+        
+        $senha = $_POST["senha"];
+		
+        include('conexao.php');
+        $sql = 'select * from usuario where ds_login = "' . $login . '" ' .
+                ' and ds_senha = "' . md5($senha) . '"';
+
+        $resultado = $conexao->query($sql);
+        if ($resultado->num_rows > 0) {
+            setcookie('login', $login, (time() + 240));
+            header('Location: profile.php?login=' . $login);
+        }else {
+            setcookie('login');
+            header('Location: profile.php');
+        }
+        ?>
+
+    </body>
+</html>
