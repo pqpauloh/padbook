af099d7571d6afb18f5edc690b1825d7f58d198a
Paulo H <Paulo H@192.168.1.2>
2016-02-05 22:37:32 -0200

    Iniciativa PadBook em versão Alfa, atualizando projeto. Resolvendo problema com Banco de dados.

--- conexao.php
+++ conexao.php
@@ -1,5 +1,5 @@
 <?php
-    $conexao = new mysqli("Global", "Root", "", "padbook");
+    $conexao = new mysqli("localhost", "u472199986_beta", "247611pauloh", "u472199986_padbk");
 	if ($conexao->connect_error) {
 		echo "Conexao com o banco falhou: " . $conexao->connect_error;
 	} 
